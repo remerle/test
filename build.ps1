3b9e8a2db478c6d6281c2ea70aa9ce08e4a2bc36
remerl200 <ryan_emerle@comcast.com>
2020-10-14 10:00:20 -0400

    Latest binaries v1.0.15

--- build.ps1
+++ build.ps1
@@ -1,13 +1,13 @@
 $ver = (get-content VERSION | out-string).Trim()
 
 Write-Output "Building Linux Binary"
-$Env:GOOS = "linux"; $Env:GOARCH = "amd64"; go build -ldflags "-X main.Version=v$ver" -o bin/core-$Env:GOOS-$Env:GOARCH
+$Env:GOOS = "linux"; $Env:GOARCH = "amd64"; go build -ldflags "-X main.Version=$ver" -o bin/core-$Env:GOOS-$Env:GOARCH
 
 Write-Output "Building macOS Binary"
-$Env:GOOS = "darwin"; $Env:GOARCH = "amd64"; go build -ldflags "-X main.Version=v$ver" -o bin/core-$Env:GOOS-$Env:GOARCH
+$Env:GOOS = "darwin"; $Env:GOARCH = "amd64"; go build -ldflags "-X main.Version=$ver" -o bin/core-$Env:GOOS-$Env:GOARCH
 
 Write-Output "Building Windows Binary"
-$Env:GOOS = "windows"; $Env:GOARCH = "amd64"; go build -ldflags "-X main.Version=v$ver" -o bin/core-$Env:GOOS-$Env:GOARCH.exe
+$Env:GOOS = "windows"; $Env:GOARCH = "amd64"; go build -ldflags "-X main.Version=$ver" -o bin/core-$Env:GOOS-$Env:GOARCH.exe
 
 Write-Output "Pushing $ver to GitHub"
 git commit -q -a -m "Latest binaries $ver"
